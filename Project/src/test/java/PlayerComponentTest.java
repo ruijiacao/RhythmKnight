51bbe1df61e542163796f0d71d8304624089247f
Liang, Albin <albin.liang123@gmail.com>
2021-06-27 21:25:24 -0400

    Update PlayerComponentTest.java
    hotfix directory issue

--- Project/src/test/java/PlayerComponentTest.java
+++ Project/src/test/java/PlayerComponentTest.java
@@ -1,15 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 
-import components.PlayerComponent;
-
-
 public class PlayerComponentTest {
-
-    private PlayerComponent cut;
 
     @BeforeEach
     public void setCuT() {
-        cut = new PlayerComponent();
     }
 
 }
