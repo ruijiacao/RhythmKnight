adf235ded24f4cb3004425b785dbd536561353f8
mlingle3@gatech.edu <mlingle@gatech.edu>
2021-06-27 22:40:00 -0400

    Added tests
    GlobalSettings, IGenerator

--- Project/src/test/java/AppMainMenuTest.java
+++ Project/src/test/java/AppMainMenuTest.java
@@ -1,14 +1,5 @@
-
-import com.almasb.fxgl.app.GameApplication;
-import javafx.scene.control.Button;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.testfx.framework.junit.ApplicationTest;
 
-import static org.testfx.assertions.api.Assertions.assertThat;
-
 public class AppMainMenuTest extends ApplicationTest {
-
 
 }
