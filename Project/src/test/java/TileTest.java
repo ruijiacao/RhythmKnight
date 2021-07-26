182630c4f739ca8898d8711e5acf567313c93eb8
Albin Liang <aliang40@gatech.edu>
2021-07-25 22:37:21 -0400

    update tileTest, playerTest

--- Project/src/test/java/TileTest.java
+++ Project/src/test/java/TileTest.java
@@ -1,6 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.texture.Texture;
 import javafx.geometry.Point2D;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit.ApplicationTest;
@@ -17,93 +21,31 @@ public class TileTest extends ApplicationTest {
     @Test
     public void testConstructor() {
         Tile tile1 = new Tile(new Point2D(405, 435), TileType.INVISIBLE);
-        Assertions.assertEquals(tile1.getPosition().getX(), 405.0);
-        Assertions.assertEquals(tile1.getPosition().getY(), 435.0);
-    }
-
-
-    // checks if a tile is properly added to a scene
-    @Test
-    void displayOnScene() {
-
-    }
-
-    // checks if a tile is properly released from a scene
-    @Test
-    void removeFromScene() {
-
-    }
-
-    // checks for no errors on calling setTileTexture()
-    @Test
-    void setTileTexture() {
-    }
-
-    // checks for no errors on calling setOpacity()
-    @Test
-    void setOpacity() {
-        //Tile tile1 = new Tile(new Point2D(405, 435), TileType.UNVISITED);
-        //Assertions.assertEquals(tile1.getPosition().getX(), 405.0);
-        //Assertions.assertEquals(tile1.getPosition().getY(), 435.0);
-        //tile1.setOpacity(.5);
-        //Assertions.assertEquals(.5, tile1.getOpacity());
-    }
-
-    // checks for no errors on calling setScale()
-    @Test
-    void setScale() {
-        //Tile tile1 = new Tile(new Point2D(405, 435), TileType.UNVISITED);
-        //Assertions.assertEquals(tile1.getPosition().getX(), 405.0);
-        //Assertions.assertEquals(tile1.getPosition().getY(), 435.0);
-        //tile1.setScale(.5);
-        //Assertions.assertEquals(.5, tile1.getScale());
-    }
-
-    // checks for no errors on calling setPlayerOnTile()
-    @Test
-    void setPlayerOnTile() {
-
-    }
-
-    // checks for no errors on calling setActive()
-    @Test
-    void setActive() {
-
+        assertEquals(tile1.getPosition().getX(), 405.0);
+        assertEquals(tile1.getPosition().getY(), 435.0);
     }
 
     // checks for the correct tile texture
     @Test
     void getTileTexture() {
-        
+        Tile tile1 = new Tile(new Point2D(405, 435), TileType.MYSTERY);
+        Texture texture = FXGL.getAssetLoader().loadTexture("newUnvisitedTile.png");
+        tile1.setTileTexture(texture);
+        assertEquals(tile1.getTileTexture(),
+            texture);
     }
 
-    // checks for correct opacity value
     @Test
-    void getOpacity() {
-        //Tile tile1 = new Tile(new Point2D(405, 435), TileType.UNVISITED);
-        //tile1.setOpacity(.5);
-        //Assertions.assertEquals(.5, tile1.getOpacity());
+    void getPosition() {
+        Tile tile1 = new Tile(new Point2D(405, 435), TileType.NORMAL);
+        Point2D pos = new Point2D(405, 435);
+        assertEquals(pos, tile1.getPosition());
     }
 
-    // checks for correct active status
     @Test
-    void isPlayerOnTile() {
-        //Tile tile1 = new Tile(new Point2D(200, 200), TileType.UNVISITED);
-        //tile1.setPlayerOnTile(true);
-        //assertTrue(tile1.isPlayerOnTile());
-    }
-
-    // checks for correct active status
-    @Test
-    void isActive() {
-        //Tile tile1 = new Tile(new Point2D(200, 200), TileType.UNVISITED);
-        //tile1.setActive(true);
-        //assertTrue(tile1.isTileActive());
-    }
-
-    // checks if all tile properties are reset back to normal
-    @Test
-    void reset() {
-
+    void isVisited() {
+        Tile tile1 = new Tile(new Point2D(405, 435), TileType.NORMAL);
+        tile1.setVisited();
+        assertTrue(tile1.isVisited());
     }
 }
--- Project/src/test/java/playerTest.java
+++ Project/src/test/java/playerTest.java
@@ -8,79 +8,90 @@ class PlayerTest extends ApplicationTest {
     private Player cut;
 
     @BeforeEach
-
+    void setCut() {
+        cut = new Player();
+    }
 
     @Test
     void getGold() {
-        cut = new Player();
         assertEquals(cut.getGold(), 0);
     }
 
     @Test
     void setGold() {
+        cut.setGold(15);
+        assertEquals(cut.getGold(), 15);
     }
 
     @Test
     void pickedUpGold() {
+        cut.pickedUpGold(15);
+        assertEquals(cut.getGold(), 15);
     }
 
     @Test
     void getMonstersSlain() {
+        cut.monsterSlain();
+        cut.monsterSlain();
+        assertEquals(cut.getMonstersSlain(), 2);
     }
 
     @Test
     void monsterSlain() {
+        assertEquals(cut.getMonstersSlain(), 0);
+        cut.monsterSlain();
+        assertEquals(cut.getMonstersSlain(), 1);
     }
 
     @Test
     void setMonstersSlain() {
-    }
-
-    @Test
-    void getHealth() {
+        assertEquals(cut.getMonstersSlain(), 0);
+        cut.setMonstersSlain(6);
+        assertEquals(cut.getMonstersSlain(), 6);
     }
 
     @Test
     void setHealth() {
+        cut.setHealth(99);
+        assertEquals(cut.getHealth(), 99);
     }
 
     @Test
     void updateHealth() {
-    }
-
-    @Test
-    void getName() {
+        cut.setHealth(100);
+        cut.updateHealth(-47);
+        assertEquals(cut.getHealth(), 53);
     }
 
     @Test
     void setName() {
-    }
-
-    @Test
-    void getWeapon() {
+        cut.setName("Test");
+        assertEquals(cut.getName(), "Test");
     }
 
     @Test
     void setWeapon() {
+        cut.setWeapon(2);
+        assertEquals(cut.getWeapon(), 2);
     }
 
-    @Test
-    void getPosition() {
-    }
-
-    @Test
-    void setPosition() {
-    }
 
     @Test
     void resetStats() {
+        cut.setGold(52);
+        cut.setMonstersSlain(5);
+        cut.updateScore(50);
+        cut.resetStats();
+        assertEquals(cut.getGold(), 0);
+        assertEquals(cut.getScore(), 0);
+        assertEquals(cut.getMonstersSlain(), 0);
+
     }
 
     @Test
     void getScore() {
-    }
-
-    @Test
-    void updateScore() {
+        assertEquals(cut.getScore(), 0);
+        cut.updateScore(9);
+        assertEquals(cut.getScore(), 9);
     }
 }
